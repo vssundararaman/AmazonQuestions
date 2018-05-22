720152a3b87e0f82c99a2a723cd67a58f903593e
e080801 <vidyasankar.sundararaman@mastercard.com>
2018-05-22 13:17:23 -0500

    Adding Missing Number Logic

---
+++ src/amazon/FindMissingNumber.java
@@ -0,0 +1,42 @@
+package amazon;
+
+/**
+ * You are given a list of n-1 integers and these integers are in the range of 1 to n.
+ * There are no duplicates in list. One of the integers is missing in the list. Write an efficient code to find the missing integer.
+ *
+ * 1. Get the sum of numbers
+ *        total = n*(n+1)/2
+ * 2  Subtract all the numbers from sum and
+ *    you will get the missing number.
+ *
+ */
+public class FindMissingNumber {
+
+    public static void main(String[] args) {
+
+
+        int[] arr1={7,5,6,1,4,2};
+
+        System.out.println("Missing Number is " + getMissingNumber(arr1));
+ //       getMissingNumber(arr1);
+    }
+
+
+    static int getMissingNumber(int[] array){
+        int result = 0;
+
+        int n = array.length+1;
+        int arraySum = 0;
+        int totalSum = n*(n+1)/2;
+        System.out.println(" total " + totalSum);
+
+        for(int value:array){
+            arraySum +=value;
+        }
+
+        result = totalSum - arraySum;
+
+        return result;
+    }
+
+}
---
+++ src/amazon/FindMissingNumberREADME.md
@@ -0,0 +1,25 @@
+Find the Missing Number
+You are given a list of n-1 integers and these integers are in the range of 1 to n. There are no duplicates in list. One of the integers is missing in the list. Write an efficient code to find the missing integer.
+
+Example :
+I/P    [1, 2, 4, ,6, 3, 7, 8]
+O/P    5
+
+
+METHOD 1(Use sum formula)
+Algorithm:
+
+1. Get the sum of numbers 
+       total = n*(n+1)/2
+2  Subtract all the numbers from sum and
+   you will get the missing number.
+   
+Time Complexity : O(n)
+   
+There can be overflow if n is large. In order to avoid Integer Overflow, we can pick one number from known numbers and subtract one number from given numbers. This way we wont have Integer Overflow ever. Thanks to Sahil Rally for suggesting this improvement.
+
+METHOD 2(Use XOR)
+
+  1) XOR all the array elements, let the result of XOR be X1.
+  2) XOR all numbers from 1 to n, let XOR be X2.
+  3) XOR of X1 and X2 gives the missing number.

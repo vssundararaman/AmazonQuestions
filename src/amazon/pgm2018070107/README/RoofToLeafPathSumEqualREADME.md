4449e3372f68f305f742c8ea462f8fe8d6fe5f6a
e080801 <vidyasankar.sundararaman@mastercard.com>
2018-07-02 13:45:21 -0500

    RootToLeafPathSumBinaryTree

---
+++ src/amazon/pgm2018070107/README/RoofToLeafPathSumEqualREADME.md
@@ -0,0 +1,15 @@
+**Root to leaf path sum equal to a given number**
+ 
+Given a binary tree and a number, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals the given number. Return false if no such path can be found.
+
+<img src="https://www.geeksforgeeks.org/wp-content/uploads/sum_property_tree1.gif"/>
+
+For example, in the above tree root to leaf paths exist with following sums.
+
+21 –> 10 – 8 – 3  
+23 –> 10 – 8 – 5  
+14 –> 10 – 2 – 2  
+
+So the returned value should be true only for numbers 21, 23 and 14.   For any other number, returned value should be false
+
+
---
+++ src/amazon/pgm2018070107/RootToLeafPathSumBinaryTree.java
@@ -0,0 +1,57 @@
+package amazon.pgm2018070107;
+
+public class RootToLeafPathSumBinaryTree {
+
+    Node root;
+
+    public static void main(String[] args) {
+
+        int sum = 21;
+
+        RootToLeafPathSumBinaryTree tree = new RootToLeafPathSumBinaryTree();
+        tree.root = new Node(10);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(2);
+        tree.root.left.left = new Node(3);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(2);
+
+        if (tree.hasPathSum(tree.root, sum))
+            System.out.println("There is a root to leaf path with sum " + sum);
+        else
+            System.out.println("There is no root to leaf path with sum " + sum);
+    }
+
+    boolean hasPathSum(Node node, int sum){
+
+        if(node == null){
+            return (sum == 0);
+        }else{
+            boolean answer = false;
+
+            int subsum = sum - node.data;
+            if(subsum ==0 && node.left == null & node.right == null){
+                return true;
+            }
+            if(node.left !=null){
+                answer = answer || hasPathSum(node.left,subsum);
+            }
+
+            if(node.right !=null){
+                answer = answer || hasPathSum(node.right,subsum);
+            }
+
+            return answer;
+        }
+   }
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data){
+        this.data = data;
+        left = right = null;
+    }
+}
